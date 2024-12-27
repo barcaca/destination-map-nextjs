c38375b4aa4ed81edd8afe68d3f6bb1400179b89
barcaca <73846225+barcaca@users.noreply.github.com>
2024-12-27 13:17:02 -0300

    feat(utils): Cria utilitário generateUniqueRandomNumbers

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateUniqueRandomNumbers(
+  count: number,
+  max: number
+): number[] {
+  if (count <= 0 || max <= 0 || count > max) {
+    throw new Error(
+      "Parâmetros inválidos: 'count' deve ser positivo e menor ou igual a 'max'."
+    )
+  }
+
+  const numbers: Set<number> = new Set<number>([])
+
+  while (numbers.size < count) {
+    const randomNumber = Math.floor(Math.random() * max)
+    numbers.add(randomNumber)
+  }
+
+  return Array.from(numbers)
+}
