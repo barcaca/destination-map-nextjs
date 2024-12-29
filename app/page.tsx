a14a756b7ea74b58e8eadb8864f93a0b74801a16
barcaca <73846225+barcaca@users.noreply.github.com>
2024-12-29 16:03:37 -0300

    feat(page): Integra componente UserPlaces na home]

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
+import { verifySession } from '@/lib/dal'
 import { Suspense } from 'react'
 import { ContinentsSkeleton, ContinentsWrapper } from './_components/continents'
 import {
   Recommendations,
   RecommendationsSkeleton,
 } from './_components/recommendations'
+import { UserPlacesWrapper } from './_components/user-places'
 
 export default async function Home() {
+  const session = await verifySession()
   return (
     <div className="bg-background text-foreground">
       <main className="space-y-16 py-16">
         <Suspense fallback={<RecommendationsSkeleton />}>
           <Recommendations />
         </Suspense>
+        {session?.isAuth && <UserPlacesWrapper userId={session.userId} />}
         <Suspense fallback={<ContinentsSkeleton />}>
           <ContinentsWrapper />
         </Suspense>
